ad8ab197a6b7a7b6dbfef29975ffa72ca713d503
shpakil <kirillkotenev32@gmail.com>
2025-09-21 04:54:14 +0300

    Reapply "added class Player"
    This reverts commit 872c5e2f94f3cd5999af8c858d24c4ced9d9de7a.

---
+++ SeaBattleKursovai/Player.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+#include <utility>   // для std::pair
+#include "Board.h"  
+
+class Player
+{
+protected:
+    std::string name;   // имя игрока
+    Board board;    // игровое поле игрока
+
+public:
+    Player(const std::string& playerName);
+
+    virtual ~Player() = default;
+
+    // Чисто виртуальные методы
+    virtual void placeShips() = 0;                     // расстановка кораблей
+    virtual std::pair<int, int> makeMove() = 0;        // выбор клетки для выстрела
+
+    // Общие методы
+    Board& getBoard();                             // получить ссылку на поле
+    const std::string& getName() const;               // имя игрока
+    bool hasLost() const;                              // проверка потопления всех кораблей
+};
+
+
